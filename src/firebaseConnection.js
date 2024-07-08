ef5104c4d02808563266a38e7993c2dcc569a986
BorgesAbraao10 <abraao10clear@gmail.com>
2024-07-07 22:21:51 -0300

    Last update

---
+++ src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbsraoaup1gx-7u5E3_RJw_8TskzqYO-s",
+  authDomain: "curso-31d14.firebaseapp.com",
+  projectId: "curso-31d14",
+  storageBucket: "curso-31d14.appspot.com",
+  messagingSenderId: "351450087877",
+  appId: "1:351450087877:web:c6899ba26e6e25dc654bac",
+  measurementId: "G-49B7BHVGHX",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
