680a076ce568b892fade56b2375d45584604c25d
BorgesAbraao10 <abraao10clear@gmail.com>
2024-07-12 10:16:49 -0300

    Last update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
 import { db } from "./firebaseConnection";
+import { doc, setDoc } from "firebase/firebase";
+import "./app.css";
 
 function App() {
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+
+  async function handleAdd() {
+    await setDoc(doc(db, "posts", "12345"), {
+      titulo: titulo,
+      autor: autor,
+    })
+      .then(() => {
+        console.log("DADOS REGISTRADO NO BANCO");
+      })
+      .catch((error) => {
+        console.log("GEROU ERRO" + error);
+      });
+  }
+
   return (
     <div>
       <h1>React JS + Firebase :) </h1>
+      <div className="container">
+        <label>Titulo:</label>
+        <textarea
+          type="text"
+          placeholder="digite o titulo"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        />
+
+        <label>Autor:</label>
+        <input
+          type="text"
+          placeholder="Autor do post"
+          value={autor}
+          onChange={(e) => setAutor(e.target.value)}
+        />
+
+        <button onClick={handleAdd}>Cadastrar</button>
+      </div>
     </div>
   );
 }
