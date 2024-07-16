64cfc29a78eb501d4739ef2e759ba2160bc81cdb
BorgesAbraao10 <abraao10clear@gmail.com>
2024-07-16 12:42:30 -0300

    Last update

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import {
   addDoc,
   getDoc,
   getDocs,
+  updateDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import "./app.css";
 
 function App() {
   const [titulo, setTitulo] = useState("");
   const [autor, setAutor] = useState("");
-
+  const [idPost, setIdPost] = useState("");
   const [posts, setPosts] = useState([]);
 
   async function handleAdd() {
@@ -71,10 +73,41 @@ function App() {
       });
   }
 
+  async function editarPost() {
+    const docRef = doc(db, "post", idPost);
+    await updateDoc(docRef, {
+      titulo: titulo,
+      autor: autor,
+    })
+      .then(() => {
+        console.log("POST ATUALIZADO!");
+        setIdPost("");
+        setTitulo("");
+        setAutor("");
+      })
+      .catch(() => {
+        console.log("ERRO AO ATUALIZAR O POST");
+      });
+  }
+
+  async function excluirPost(id) {
+    const docRef = doc(db, "post", id);
+    await deleteDoc(docRef).then(() => {
+      alert("POST DELETADO COM SUCESSO...");
+    });
+  }
+
   return (
     <div>
       <h1>React JS + Firebase :) </h1>
       <div className="container">
+        <label>ID do Post:</label>
+        <input
+          placeholder="Digite o ID do post"
+          value={idPost}
+          onChange={(e) => setIdPost(e.target.value)}
+        />{" "}
+        <br />
         <label>Titulo:</label>
         <textarea
           type="text"
@@ -82,7 +115,6 @@ function App() {
           value={titulo}
           onChange={(e) => setTitulo(e.target.value)}
         />
-
         <label>Autor:</label>
         <input
           type="text"
@@ -90,16 +122,20 @@ function App() {
           value={autor}
           onChange={(e) => setAutor(e.target.value)}
         />
-
         <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscarPost}>Buscar post</button>
-
+        <button onClick={buscarPost}>Buscar post</button> <br />
+        <button onClick={editarPost}>Atualizar post</button>
         <ul>
           {posts.map((post) => {
             return (
               <li key={post.id}>
+                <strong>ID: {post.id}</strong> <br />
                 <span>Titulo: {post.titulo}</span> <br />
-                <span>Autor: {post.autor}</span> <br /> <br />
+                <span>Autor: {post.autor}</span> <br />
+                <button onClick={() => excluirPost(post.id)}>
+                  Excluir
+                </button>{" "}
+                <br /> <br />
               </li>
             );
           })}
